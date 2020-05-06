d51d619c3f7cd51a98a096fc677f9b61f4c52b67
Brett Andrews <brettandrews3@gmail.com>
2020-05-06 12:26:20 -0500

    Made changes

---
+++ BestBuyBestPractices/DapperProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Dapper;
+namespace BestBuyBestPractices
+{
+    public class DapperProductRepository : IProductRepository
+        new DapperProductRepository()
+    {
+        public static GetAllProducts();
+        public CreateProduct();
+    }
+        
+}
